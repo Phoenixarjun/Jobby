a7898baa8aaa799245f1c8f90a0de30ecaa7c968
Naresh <phoenixarjun007@gmail.com>
2025-05-24 23:23:12 +0530

    ...

--- src/components/JobItemDetails/index.js
+++ src/components/JobItemDetails/index.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useCallback} from 'react'
+import {useState, useEffect, useCallback} from 'react'
 import './index.css'
 import {useParams} from 'react-router-dom'
 import Cookies from 'js-cookie'
